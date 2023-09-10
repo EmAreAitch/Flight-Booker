4f00a89713d33b1a1ff21907b8cab122bd9051c9
EmAreAitch <rahibhasan95@gmail.com>
2023-09-10 17:35:03 +0530

    Added FlatPickr

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,13 +3,14 @@ class FlightsController < ApplicationController
     if params[:search].present?
       @search = search_param()
       if (1..4).include?(@search[:passenger_count].to_i)
-      @flight = Flight.where("departure_airport_id = ? AND arrival_airport_id = ? AND DATE(departure_time) = ?",@search[:departure], @search[:arrival], @search[:departure_date])
-      @number_of_flights = @flight.size
+        @flight = Flight.where("departure_airport_id = ? AND arrival_airport_id = ? AND DATE(departure_time) = ?", @search[:departure], @search[:arrival], @search[:departure_date])
+        @number_of_flights = @flight.size
       else
         flash.now[:alert] = "Number of passengers must be between 1 and 4"
       end
     end
-    @airports = Airport.all.map{ |airport| ["#{airport.name} - #{airport.location}",airport.id] }
+    @airports = Airport.all.map { |airport| ["#{airport.name} - #{airport.location}", airport.id] }
+    @dates = Flight.pluck(:departure_time).map(&:to_date).uniq
   end
 
   private
